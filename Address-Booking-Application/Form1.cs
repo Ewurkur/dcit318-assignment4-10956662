aaed3e76a2d74406a8ac889c9d90be1781144555
Ewurkur <ekuranchie-asiedu@st.ug.edu.gh>
2024-07-09 22:28:18 +0000

    Address booking application

---
+++ Address-Booking-Application/Form1.cs
@@ -0,0 +1,15 @@
+namespace Address_Booking_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Your information is saved successfully");
+        }
+    }
+}
